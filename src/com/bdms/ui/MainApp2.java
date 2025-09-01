5c0ad6269b861d216b4a3708ccffd0d0d280f9c0
nadiya623582 <nadiyanasrin328@gmail.com>
2025-09-01 17:51:04 +0530

    MainApp.java
    implement UI option for donor registration inside MainApp.

--- src/com/bdms/ui/MainApp2.java
+++ src/com/bdms/ui/MainApp2.java
@@ -1,11 +1,11 @@
+
 package com.bdms;
 
 import com.bdms.dao.DonorDAO;
 import com.bdms.model.Donor;
-
 import java.util.List;
 
-public class MainApp {
+public class DonorTestApp {
     public static void main(String[] args) {
         DonorDAO dao = new DonorDAO();
         List<Donor> donors = dao.getAllDonors();
